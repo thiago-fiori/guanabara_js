ac6132185da06ba99c212e3b7ce69e02e052a209
thiago-fiori <thiagofiori12@gmail.com>
2020-04-06 11:11:35 -0300

    muda cor do texto
    nao pega todas as ids

--- DOM/script.js
+++ DOM/script.js
@@ -1,4 +1,8 @@
 function clica(){
-  var p1 = document.getElementsByTagName('h1');
+  var p1 = document.getElementById('azul');
   p1.style.color = 'blue';
+  var p2 = document.getElementById('amarelo');
+  p2.style.color = 'yellow';
+  var p3 = document.getElementById('verde');
+  p3.style.color = 'green';
 }
